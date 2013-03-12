3791635da6d6a2128e92a1f42793946c02ffc380
Victor Liu <victor@victorliu.com>
2013-03-12 17:37:58 -0400

    Broke out code sample

--- QuickDialer/NewCust.vb
+++ QuickDialer/NewCust.vb
@@ -1,28 +1,29 @@
-﻿Public Class NewCust
+﻿'NewCust.vb is the form for collecting new customer contact info.
 
+Public Class NewCust
+
+
+
+    'Public variables to pass back to main form
     Public sNewNum As String
     Public sNewName As String
-    Public sNewCust As String
+    Public sNewCust As String 'Will store the contact's current organization, whether or not it's a new organization.
     Public isNewContact As Boolean
     Public isNewCust As Boolean
 
 
-
-
-
-
     Private Sub NewCust_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         txtNewPhone.Text = sNewNum
-        cboCustList.Items.AddRange(CustList)
+        cboCustList.Items.AddRange(CustList) 'Load existing organization list
     End Sub
 
 
     Private Sub chkNewCust_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkNewCust.CheckedChanged
+        'Control form layout based on "New Customer" checkbox
         If chkNewCust.Checked Then
             cboCustList.Visible = False
             cboCustList.Enabled = False
             cboCustList.SelectedIndex = -1
-            'will this -1 work?
 
             txtNewCust.Visible = True
             txtNewCust.Enabled = True
@@ -47,7 +48,7 @@
     End Sub
 
     Private Sub cmdSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSave.Click
-        If txtNewCust.Text = "" Then
+        If txtNewCust.Text = "" Then 'If contact belongs to a new organization.
             If chkNewCust.Checked Then
                 MessageBox.Show("Customer field empty")
                 Exit Sub
@@ -71,6 +72,7 @@
             sNewCust = cboCustList.SelectedItem
 
         End If
+
         isNewContact = True
 
         'MessageBox.Show(sNewCust & sNewName & sNewNum)
